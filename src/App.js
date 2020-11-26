37a5b5b78a1bb5f2ba42c8536ff384926100674b
JAlejandroMG <jalejandromg@tutanota.com>
2020-11-26 12:20:48 -0600

    Added: Cards color and animation, loader, pagination pokeball,

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from './components/card/card';
 import Pagination from './components/pagination';
 import './App.css';
+import pokeTypes from './databases/pokeTypes';
+import pokeColors from './databases/pokeColors';
 
 
 export default class App extends React.Component {
@@ -16,22 +18,17 @@ export default class App extends React.Component {
     } //Hay un máximo de 893 imágenes de Pokemon disponibles y pokemonPerPage debe ser múltiplo de 3
 
 
-  componentDidMount() {
+  async componentDidMount() {
     const limit = this.state.pokemonPerPage;
     const url = 'https://pokeapi.co/api/v2/pokemon';
     //Consumir la API de pokeapi
-    fetch(`${url}?limit=${limit}`)
-      .then(response => {
-        // console.log(response);
-        return response.json();
-      })
-      .then(data => {
-        // console.log(data).
-        this.setState({pokemones: data.results})
-      })
-      .catch( error => {
-        console.log(error);
-      })
+    try {
+      const response = await fetch(`${url}?limit=${limit}`);
+      const data = await response.json();
+      await this.setState({pokemones: data.results});
+    } catch(error) {
+        console.log(error.message);
+    }
   }
 
   getNumber = (index) => {
@@ -39,13 +36,13 @@ export default class App extends React.Component {
     return id;
   }
 
-  getImage = (index) => {
+  getImage = (id) => {
     let pokemonImg = "";
     //La imagen para el Pokemon con id 11 y nos encontramos en la segunda página
     //0 -> {"name":"metapod","url":"https://pokeapi.co/api/v2/pokemon/11/"}
     //          0   + 1  + (( 2 - 1 ) * 10) = 11
     //          0   + 1  + (( 3 - 1 ) * 10) = 21
-    let id = (index + 1) + ((this.state.currentPage - 1) * this.state.pokemonPerPage);
+    // let id = (index + 1) + ((this.state.currentPage - 1) * this.state.pokemonPerPage); // Se recibía index
 
     if(id < 10){
         pokemonImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${id}.png`;
@@ -71,21 +68,38 @@ export default class App extends React.Component {
           })
   }
 
+  updatePokemonPerPage = (event) => {
+    const newValue = parseInt(event.target.value);
+    this.setState({pokemonPerPage: newValue});
+    // event.preventDefault();
+  }
+
     render() {
         return (
           <div className="container">
             <div className="pokedex-header">
-              <input></input>
+              {/* <div className="input">
+                <form>
+                  <input value={`${this.state.pokemonPerPage}`} type="number" onChange={this.updatePokemonPerPage}></input>
+                </form>
+              </div> */}
             </div>
             <div className="pokedex-container">
               {                
                 this.state.pokemones.map( (pokemon, index) => {      
                   //2. Solucionar el problema de obtener las imagenes de los pokemones con id < 10, > 10, > 100
-                  let pokemonNumber = this.getNumber(index);
-                  let pokemonImg = this.getImage(index);
+                  const pokemonNumber = this.getNumber(index);
+                  const pokemonImg = this.getImage(pokemonNumber); // Se usaba index
+                  const pokemonTypes = pokeTypes[pokemonNumber-1];
+                  const pokemonColors = pokemonTypes.map(type => {            
+                                          return pokeColors[type]; // No funciona como pokeColors.type
+                                        });
+                  
+                  let colors = [];
+                  pokemonColors.length===1 ? colors = [pokemonColors[0], pokemonColors[0]] : colors = [pokemonColors[0], pokemonColors[1]] ;
                   // let pokemonImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${index + 1}.png`
                   return (
-                    <Card key={index + 1} number={pokemonNumber} name={pokemon.name} img={pokemonImg} />
+                    <Card key={index + 1} number={pokemonNumber} name={pokemon.name} img={pokemonImg} colors={colors} />
                   )
                 })
               }
